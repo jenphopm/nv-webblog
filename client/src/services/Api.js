e3bf4a8ecbcd5b3b39cedc87e0f07468f8ce6ce7
jenphopm <jane_auto@hotmail.com>
2019-04-04 15:39:14 +0700

    update deploy

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,8 @@ import store from '@/store'
 
 export default () => {
     return axios.create({
-        baseURL: 'http://localhost:8081/',
+        // baseURL: 'http://localhost:8081/',
+        baseURL: 'http://ec2-3-1-205-161.ap-southeast-1.compute.amazonaws.com:8081/',
         headers: {
             Authorization: `Bearer ${store.state.token}`
         }
